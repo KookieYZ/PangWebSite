c681062b6c6af4fd7a185643fc6d57b93cedbc97
Yi Huai <yihuai1234@gmail.com>
2021-10-25 23:03:02 +0800

    push latest project 2nd

--- database/migrations/2021_10_25_143250_create_persons_table.php
+++ database/migrations/2021_10_25_143250_create_persons_table.php
@@ -15,6 +15,14 @@ class CreatePersonsTable extends Migration
     {
         Schema::create('persons', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('avatar');
+            $table->string('spouse_name');
+            $table->string('spouse_avatar');
+            $table->string('gender');
+            $table->string('state');
+            $table->string('gender');
+
             $table->timestamps();
         });
     }
