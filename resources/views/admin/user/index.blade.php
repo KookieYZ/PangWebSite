a6450d2cdd094ec206cbbbe418d49fe5e6b71721
Yi Huai <yihuai1234@gmail.com>
2021-11-13 14:50:08 +0800

    YH211113 fix some page link wrongly

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -73,7 +73,7 @@
             <!-- ============================================================== -->
             <!-- Logo -->
             <!-- ============================================================== -->
-            <a class="navbar-brand" href="index.html">
+            <a class="navbar-brand" href="{{ route('admin.dashboard') }}">
               <!-- Logo icon -->
               <b class="logo-icon ps-2">
                 <!--You can put here icon as well // <i class="wi wi-sunset"></i> //-->
