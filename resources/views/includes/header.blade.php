07279e37502a853fe56517c6016c86c5f34c75b0
weijie <weijie050301@gmail.com>
2021-11-13 23:03:19 +0800

    added scrollable navigation bar at search page

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -101,7 +101,3 @@
     </ul>
   </div>  
 </nav>
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/js/bootstrap.bundle.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/js/bootstrap.min.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
