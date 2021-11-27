194f88a6c98ea1cb2d356462cc6d22dc600871d5
Tan Yong Zhi <tanyongzhi@altecflex.com>
2021-11-27 20:59:36 +0800

    blog page resize

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -2,32 +2,26 @@
 
 @section('content')
 <section class="title">
-    <div class="text-center h1 p-2 mt-5"><b><ins>延年公家訓</ins></b></div>
+    <div class="text-center h1 p-auto mt-5"><b><ins>延年公家訓</ins></b></div>
 </section>
 
 <section class="content">
-    <div class="container">
-        <div class="row">
-        @for ($i = 0; $i < 4; $i++)
-            @if ($i < 2)
-            <div class="col-xl-5 col-md-5 col-sm-12 p-4 d-flex justify-content-center">
-                <image src="{{asset('assets/images/profile.jpg')}}" height="310" width="400">
-            </div> 
-            <div class="col-xl-7 col-md-7 col-sm-12 p-1 mt-3">
-                <span>photo description: Once upon a time</span>
-            </div>        
-            @else
-            <div class="col-xl-5 col-md-5 col-12 p-4 d-flex justify-content-center">
-                <div class="embed-responsive embed-responsive-16by9">
-                <iframe class="embed-responsive-item" width="560" height="315" src="{{asset('assets/videos/doremon.mp4')}}" title="URL video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                </div>
-            </div> 
-            <div class="col-xl-7 col-md-7 col-12 p-1 mt-3">
-                <span>video description: I wanna dance, the music got me going, ain't nothing that can stop how we move yeah</span>
-            </div> 
-            @endif                         
-        @endfor
-        </div>      
+    <div class="row">
+    @for ($i = 0; $i < 4; $i++)
+        @if ($i < 2)
+        <div class="w-auto p-2 ml-3 d-flex justify-content-start">
+            <image src="{{asset('assets/images/profile.jpg')}}" height="400" width="600">
+        </div>        
+        @else
+        <div class="w-auto p-2 ml-3 d-flex justify-content-start">                
+            <iframe class="embed-responsive-item" width="400" height="315" src="{{asset('assets/videos/doremon.mp4')}}" title="URL video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>             
+        </div>
+        @endif 
+        <div class="col-xl-6 col-md-6 col-12 p-1 mt-1 ml-3">
+            <div>{{ $i < 2 ?"Photo" :"Video"}} description: <br> This page shares my best articles to read on topics like health, happiness, creativity, productivity and more. The central question that drives my work is, “How can we live better?” To answer that question, I like to write about science-based ways to solve practical problems. You’ll find interesting articles to read on topics like how to stop procrastinating as well as personal recommendations like my list of the best books to read and my minimalist travel guide. Ready to dive in? You can use the categories below to browse my best articles.</div>
+        </div>                         
+    @endfor
+    </div>      
 </section>
 
 <div class="section position-relative">
