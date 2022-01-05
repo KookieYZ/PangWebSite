2f7ef88e7d40bd9e0beccbebde0f1d2068422ba5
weijie <weijie050301@gmail.com>
2022-01-05 23:00:25 +0800

    update ThemeController

--- app/Http/Controllers/user/ThemeController.php
+++ app/Http/Controllers/user/ThemeController.php
@@ -9,15 +9,15 @@ use App\Models\Variable;
 class ThemeController extends Controller
 {
     public function facebook() {
-        $facebook = Variable::where('key', 'facebook-link')->first();
+        $facebook = Variable::where('id', 7)->first();
         return $facebook;
     }
     public function message() {
-        $facebook = Variable::where('key', 'message-link')->first();
+        $facebook = Variable::where('id', 8)->first();
         return $facebook;
     }
     public function whatapps() {
-        $facebook = Variable::where('key', 'whatsapp-link')->first();
+        $facebook = Variable::where('id', 9)->first();
         return $facebook;
     }
 }
