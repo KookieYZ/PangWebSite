d1fc607faec9772ebbc6bc42700828a28f92ea59
User <lauqiyuan99@gmail.com>
2022-01-06 18:52:00 +0800

    Update bg card color using themecontroller

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -16,14 +16,14 @@
             </form>
         </div>
     </section>
-
+    @inject('theme', 'App\Http\Controllers\User\ThemeController')
     <section class="content">   
         <div class="container mt-3 mb-3 p-2 d-flex"> 
             <div class="row d-flex justify-content-start">
             @foreach($search_persons as $search_person)
                 <div class="col-xl-3 col-md-4 col-12 p-2">
                     <a href="{{ route('site.chart') }}">
-                        <div class="card p-3 bg-danger text-white">
+                        <div class="card p-3 text-white" style="background-color:  {{ $theme->secondColor()->value }};">
                             <div class="image d-flex flex-column justify-content-center align-items-center"> <img src="{{ asset('image/avatar/'.$search_person->avatar) }}" height="100" width="100" /> 
                                 <div class="container bg-white text-dark mt-2" style="border-radius: 20px">
                                     <div class="row d-flex justify-content-center p-2">
