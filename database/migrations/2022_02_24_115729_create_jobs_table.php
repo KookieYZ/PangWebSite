652e8c7e75a036f72943796bbe92f360f0d383b4
weijie <weijie050301@gmail.com>
2022-02-24 21:53:34 +0800

    added jobs table migration file

---
+++ database/migrations/2022_02_24_115729_create_jobs_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJobsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->string('note')->nullable();
+            $table->string('image_path')->nullable();
+            $table->string('category')->nullable();
+            $table->string('salary')->nullable();
+            $table->string('background')->nullable();
+            $table->string('address')->nullable();
+            $table->date('posted_on')->nullable();
+            $table->boolean('status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jobs');
+    }
+}
