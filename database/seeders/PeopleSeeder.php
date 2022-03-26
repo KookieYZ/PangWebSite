9469e2fdcaec266f7a8a830249343a2842c26a41
weijie <weijie050301@gmail.com>
2022-03-27 00:01:54 +0800

    update people seeder

--- database/seeders/PeopleSeeder.php
+++ database/seeders/PeopleSeeder.php
@@ -16,21 +16,23 @@ class PeopleSeeder extends Seeder
      */
     public function run()
     {
-        for ($i=0; $i < 15; $i++) {
-            DB::table('people')->insert([
-                'name' => Str::random(10),
-                'avatar' => 'noimage.jpg',
-                'spouse_name' => Str::random(10),
-                'spouse_avatar' => 'noimage.jpg',
-                'gender' => 1,
-                'state' => Str::random(10),
-                'nationality' => Str::random(10),
-                'dob_date' => '2000-01-01',
-                'parent_id' => 1,
-                'era' => '第一代',
-                'created_at' => date('Y-m-d H:i:s'),
-                'updated_at' => date('Y-m-d H:i:s'),
-            ]);
+        for ($parent_id=0; $parent_id<3; $parent_id++) {
+            for ($i=0; $i <= $parent_id * 2 ; $i++) {
+                DB::table('people')->insert([
+                    'name' => Str::random(10),
+                    'avatar' => 'noimage.jpg',
+                    'spouse_name' => Str::random(10),
+                    'spouse_avatar' => 'noimage.jpg',
+                    'gender' => 1,
+                    'state' => Str::random(10),
+                    'nationality' => Str::random(10),
+                    'dob_date' => '2000-01-01',
+                    'parent_id' => $parent_id == 0 ? null : $parent_id,
+                    'era' => '第一代',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ]);
+            }
         }
     }
 }
