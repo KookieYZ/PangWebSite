0540ac29102d53dc8b712d67236f996f7a69a9db
weijie <weijie050301@gmail.com>
2022-06-30 18:23:00 +0800

    set user site icon

--- resources/views/includes/app.blade.php
+++ resources/views/includes/app.blade.php
@@ -11,7 +11,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>彭氏公会</title>
+    <title>{{ config('app.name', '彭氏公会') }}</title>
+    <link
+    rel="icon"
+    type="image/png"
+    sizes="16x16"
+    href="{{ asset('image/PANG_CLAN_LOGO.png') }}"/>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
