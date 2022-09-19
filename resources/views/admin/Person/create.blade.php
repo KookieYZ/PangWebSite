431eaec78e003780577a95837d2652ba8eeb553d
JL <lunhor1999@gmail.com>
2022-09-19 16:06:51 +0800

    Add CSS Class

--- resources/views/admin/Person/create.blade.php
+++ resources/views/admin/Person/create.blade.php
@@ -102,7 +102,7 @@
                             <div class="form-group row">
                                 <label class="col-sm-3 text-end control-label col-form-label hidden"></label>
                                 <div class="col-md-9">
-                                    <a href="" id="addmorespouse" name="addmorespouse" class="link-primary"
+                                    <a href="" id="addmorespouse" name="addmorespouse" class="link-primary btn btn-primary"
                                         title="添加配偶">{{ __('添加配偶') }}</a>
                                 </div>
                             </div>
